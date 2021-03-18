db299af7c8b290070acb310ab40f9d6d43bd217e
Aleksi <aleksivaina@hotmail.com>
2021-03-18 15:24:21 +0200

    Commit jeejee

--- src/main/java/fi/vamk/studentid/App.java
+++ src/main/java/fi/vamk/studentid/App.java
@@ -1,5 +1,7 @@
 package fi.vamk.studentid;
 
+import java.text.DecimalFormat;
+
 /**
  * Hello world!
  *
@@ -7,6 +9,44 @@ package fi.vamk.studentid;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("1 divided by 0 is " + divide(1,0));
     }
 
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+    * Function get two doubles as arguments and return the 
+    * division of them in two decimals. If denominator is zero return NaN.
+    */
+
+    public static double divide (double a, double b) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        if (b == 0){
+            return Double.NaN;
+        } else {
+            return Double.valueOf(df.format(a/b).replace(",",".")).doubleValue();
+        }
+    }
+
+    public static String returnTwoChars(String input){
+        if (input != null){
+            if(input.length() < 2){
+                return input;
+            } else {
+                return input.substring(0, 2);
+            }
+        } else{
+        return null;
+        }
+    }
+
+    public static Double div(Double a, Double b) {
+        if(a != null && b != null && b != 0) {
+            return a/b;
+        } else {
+            return null;
+        }
+    }
 }
--- src/test/java/fi/vamk/studentid/AppTest.java
+++ src/test/java/fi/vamk/studentid/AppTest.java
@@ -16,4 +16,36 @@ public class AppTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+
+    @Test
+    public void addTest(){
+        assertEquals(10, App.add(6,4));
+        assertEquals(0, App.add(0,0));
+        assertEquals(0, App.add(-6,6));
+    }
+
+    @Test
+    public void divideTest(){
+        assertEquals(1.33d, App.divide(4.0d,3.0d), 0);
+        assertEquals(0.0d, App.divide(0.0d,1.0d), 0);
+        assertEquals(Double.NaN, App.divide(5.0d, 0.0d), 0);
+    }
+
+    @Test
+    public void returnTwoCharsTest(){
+        assertEquals("qw", App.returnTwoChars("qwerty"));
+        assertEquals("VA", App.returnTwoChars("VA"));
+        assertEquals("V", App.returnTwoChars("V"));
+        assertEquals("", App.returnTwoChars(""));
+        assertEquals(null, App.returnTwoChars(null));
+    }
+
+    @Test
+    public void divTest(){
+        assertEquals(Double.valueOf(1.33), App.div(new Double(4), new Double(3)), .2);
+        assertEquals(Double.valueOf(33), App.div(new Double(99), new Double(3)), .2);
+        assertEquals(null, App.div(null, new Double(3)));
+        assertEquals(null, App.div(new Double(3), null));
+        assertEquals(null, App.div(new Double(3), new Double(0)));
+    }
 }
